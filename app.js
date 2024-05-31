01c8e9fc36769a8f25693fcc45312db1e449e411
yisoo song <yisoo1049@gmail.com>
2024-06-01 03:25:31 +0900

    login update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const indexRouter = require('./routes/index')
 const app = express()
 
 require('dotenv').config();
@@ -9,12 +10,15 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json()) //req.body가 객체로 인식이 된다
 
-const mongoURI = process.env.LOCAL_DB_ADDRESS;
+app.use('/api',indexRouter);
+// api/user 이런 주소로 오면 indexRouter로 감. 즉, 무조건 접속하면 indexRouter로 간다.
+
+const mongoURI = process.env.MONGODB_URI_ADDRESS;
 // mongoose.connect(mongoURI,{useNewUrlParser:true})
 mongoose.connect(mongoURI)
 .then(()=>console.log("mongoose connected"))
 .catch((err)=>console.log("DB connection fail", err));
 
-app.listen(process.env.PORT||5000,()=>{
+app.listen(process.env.PORT || 5001,()=>{
     console.log("server on");
 })
