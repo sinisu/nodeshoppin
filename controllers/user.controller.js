a5e6e687551069a1917f62cd05234b9c883112ed
yisoo song <yisoo1049@gmail.com>
2024-06-03 02:05:00 +0900

    token inout

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,4 +20,17 @@ userController.createUser = async(req,res) => {
     }
 };
 
+userController.getUser = async(req,res)=>{
+    try{
+        const {userId} = req;
+        const user = await User.findById(userId);
+        if(user){
+            res.status(200).json({status:"success",user});
+        }
+        throw new Error("Invalid token");
+    }catch(error){
+        res.status(400).json({status:"error",error:error.message});
+    }
+};
+
 module.exports = userController;
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
+const authController = require('../controllers/auth.controller');
 const router = express.Router()
 
 //회원가입
 router.post('/',userController.createUser)
+router.get('/me',authController.authenticate,userController.getUser) //토큰이 valid한 토큰인지, 이 token을 가지고 유저를 찾아서 리턴
 
 module.exports = router;
