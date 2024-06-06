e984859d6dbbbda8a3414918b51e4ace5b2d3388
yisoo song <yisoo1049@gmail.com>
2024-06-07 05:03:36 +0900

    search update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+const PAGE_SIZE = 5;
 const productController = {}
 
 productController.createProduct = async(req,res) =>{
@@ -15,8 +16,38 @@ productController.createProduct = async(req,res) =>{
 
 productController.getProducts = async(req,res) => {
     try{
-        const products = await Product.find({});
-        res.status(200).json({status:"success",data:products});
+        const {page,name,pagesize} = req.query;
+        const cond = name?{name:{$regex:name,$options:'i'}}:{};
+        let query = Product.find(cond);
+        let response = {status:"success"};
+        // 아래 방식은 조건이 늘어날 때 마다 구문을 새로 써야 하므로 비효율적
+        // if(name){
+        //     const product = await Product.find({name:{
+        //         $regex:name, // name을 포함한 정보를 검색
+        //         $option:"i" // 대문자, 소문자 구분하지 않겠다는 뜻
+        //     }});
+        // }else{
+        //     const products = await Product.find({});
+        // }
+
+        if(page){
+            // skip,limit은 mongoose의 함수
+            // skip은 data값을 skip해 줌
+            // limit은 최대 몇 개씩 보여줄 것인지 
+            query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE);
+            // 전체 페이지
+            // 데이터 총 개수
+            const totalItemNum = await Product.find(cond).count();
+            // 데이터 총 개수 / PAGE_SIZE
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            response.totalItemNum = totalItemNum;
+            response.pageSize = PAGE_SIZE;
+            response.totalPageNum = totalPageNum;
+        }
+
+        const productList = await query.exec(); // query의 선언과 exec로 실행을 따로 함
+        response.data = productList;
+        res.status(200).json(response);
     }catch(error){
         res.status(400).json({status:"fail",error:error.message});
     };
