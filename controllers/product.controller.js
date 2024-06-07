c4def54d5b13bece967dfb6ee80c1fc883da54fd
yisoo song <yisoo1049@gmail.com>
2024-06-08 00:26:19 +0900

    file delete

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -53,4 +53,20 @@ productController.getProducts = async(req,res) => {
     };
 }
 
+productController.updateProduct = async(req,res) => {
+    try{
+        const productId = req.params.id;
+        const {sku,name,size,image,category,description,price,stock,status} = req.body;
+        const product = await Product.findByIdAndUpdate(
+            {_id:productId},
+            {sku,name,size,image,category,description,price,stock,status},
+            {new:true}
+        );
+        if(!product) throw new Error("item doesn't exist");
+        res.status(200).json({status:"success", data: product});
+    }catch(error){
+        res.status(400).json({status:"fail",error:error.message});
+    }
+}
+
 module.exports = productController;
--- routes/product.api.js
+++ routes/product.api.js
@@ -6,10 +6,17 @@ const router = express.Router();
 //상품을 아무나 등록하지 못하게 하기 위해 권한 확인
 router.post('/',
 //퍼미션 체크 확인 전에 유저 아이디를 확인하면 됨
-authController.authenticate,
-authController.checkAdminPermission,
-productController.createProduct);
+    authController.authenticate,
+    authController.checkAdminPermission,
+    productController.createProduct
+);
 
-router.get('/',productController.getProducts)
+router.get('/',productController.getProducts);
+
+router.put('/:id',
+    authController.authenticate,
+    authController.checkAdminPermission,
+    productController.updateProduct
+);
 
 module.exports = router;
